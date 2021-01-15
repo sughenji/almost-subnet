1d5be3e2fa3e1802241657675ab0e5057f4be72c
sughenji <sugo@olografix.org>
2021-01-16 00:27:08 +0100

    some improvements

--- as.py
+++ as.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
 
-import re,ipaddress
+import sys
+import re
+import ipaddress
+
+if len(sys.argv) != 2:
+	print('\nUsage: ./as.py FILE\n')
+	exit('Exiting...')
+file = open(sys.argv[1],'r')
+
+for line in file.readlines():
+	line = line.strip('\n')
+	try:
+		ipaddress.ip_address(line)
+		print('IP ' + str(line) + ' is a valid IPv4 address')
+		print('IP ' + str(line) + ' is in subnet: ' + str(ipaddress.ip_network('192.168.0.8/30')))
+	except ValueError:
+		print('IP ' + str(line) + ' is NOT a valid address')
 
 #x = [ "192.168.0.1", "192.168.0.2", "192.168.0.3", "192.168.0.4" ]
 #y = [ "192.168.0.1", "192.168.0.5" ]
@@ -8,21 +24,6 @@ import re,ipaddress
 #result = re.findall([y], [x])
 #result = x + y
 #print(result)
-
-#mask = 30
-#ip = 192.168.0.2
-
-#network = ipaddress.ip_network('192.168.0.2/30')
-#print(network)
-
-#list = [0, 0, 0, 0]
-#for i in range(0,4):
-#	list[i] = i+1
-#
-#print(list)
-#otherlist = [2]
-#
-#result = re.findall
 
 list(ipaddress.ip_network('192.168.0.0/29').hosts())
 print(list)
