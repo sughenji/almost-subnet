0e59c2db64b2e469c055c51bee500fc39a7d93bd
sughenji <sugo@olografix.org>
2021-01-16 19:14:54 +0100

    fix some bug, use new_prefix builtin function

--- as.py
+++ as.py
@@ -4,29 +4,22 @@ import sys
 import re
 import ipaddress
 
-# Define bits/prefix dictionary
-bitprefix_dict = { "32":0, "31":1, "30":2, "29":3, "28":4, "27":5, "26":6, "25":7 }
-
 if len(sys.argv) != 2:
 	print('\nUsage: ./as.py FILE\n')
 	print('Example: ./as.py networks.txt\n')
 	exit('Exiting...')
 
-userprefix = input('\nEnter desidered prefix (eg. 29): ')
+prefix = input('\nEnter desidered prefix (eg. 29): ')
 
 try:
-	val = int(userprefix)
+	val = int(prefix)
 except ValueError:
    	exit('\n[X] Invalid user input!\nExiting...\n')
 
-if int(userprefix) > 32 or int(userprefix) < 25:
+if int(prefix) > 32 or int(prefix) < 25:
 	exit('\n[X] Unsupported prefix!\n')
 
 file = open(sys.argv[1],'r')
-
-# Convert prefix in bit for later use with ipaddress module
-prefix = bitprefix_dict.get(userprefix)
-#print(prefix)
 
 # Initialize empty list
 listips = [] 
@@ -43,7 +36,6 @@ for line in file.readlines():
 		listips.append(ipaddress.ip_address(line))
 		# Replace last octet with 0, converting in /24 network address
 		line = line[:line.rfind('.')+1] + '0/24'
-		#line = line[:line.rfind('.')+1] + '0'
 		# Add /24 networks to a set
 		netset.add(line) 
 	except ValueError:
@@ -51,6 +43,6 @@ for line in file.readlines():
 		exit('\n[X] Please sanitize your file first.')
 print('\n[*] File is OK!')
 
-print('\n[*] Generating /' +str(userprefix) + ' subnets...\n')
+print('\n[*] Generating /' +str(prefix) + ' subnets for every network...\n')
 for addr in netset:
-	print(list(ipaddress.ip_network(addr).subnets(prefixlen_diff=prefix)))
+	print(list(ipaddress.ip_network(addr).subnets(new_prefix=int(prefix))))
